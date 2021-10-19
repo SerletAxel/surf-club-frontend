dfe670e884559fb32640ffb8e46b821fc3bb8226
jean-dedieu <jeandedieu.twagirumuhoza@gmail.com>
2021-10-19 09:47:43 +0200

    Implemented front into React

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './NavigationItems.css';
+
+const navItems = [
+  { id: 'feed', text: 'Boutique', link: '/', auth: true },
+  { id: 'login', text: 'Se connecter', link: '/', auth: false },
+  { id: 'signup', text: 'S\'inscrire', link: '/signup', auth: false }
+];
+
+const navigationItems = props => [
+  ...navItems.filter(item => item.auth === props.isAuth).map(item => (
+    <li
+      key={item.id}
+      className={['navigation-item', props.mobile ? 'mobile' : ''].join(' ')}
+    >
+      <NavLink to={item.link} exact onClick={props.onChoose}>
+        {item.text}
+      </NavLink>
+    </li>
+  )),
+  props.isAuth && (
+    <li className="navigation-item" key="logout">
+      <button onClick={props.onLogout}>Déconnexion</button>
+    </li>
+  )
+];
+
+export default navigationItems;
